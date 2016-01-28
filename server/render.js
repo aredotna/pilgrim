340b951f12d81349fb5a303c0a8d1043d6a03147
Charles Broskoski <broskoski@gmail.com>
2016-01-28 08:14:20 -0500

    point to generated assets

--- server/render.js
+++ server/render.js
@@ -1,17 +1,18 @@
 export default (html, initialState) =>  {
+  const assets = JSON.parse(fs.readFileSync(path.join(__dirname, '../assets.json'), 'utf8'));
   return `
     <!doctype html>
     <html>
       <head>
         <title>Pilgrim by Are.na</title>
-        <link href="/main.css" rel="stylesheet" type="text/css"/>
+        <link href=""${assets.main.css}" rel="stylesheet" type="text/css"/>
       </head>
       <body>
         <div id="root">${html}</div>
         <script>
           window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}
         </script>
-        <script src="/main.js"></script>
+        <script src="${assets.main.js}"></script>
       </body>
     </html>
     `
