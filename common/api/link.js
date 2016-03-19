8a8a1b8f03a9277e911c202a13880da2f27eaf65
Charles Broskoski <broskoski@gmail.com>
2016-03-19 16:08:21 -0400

    clean up npm package

--- common/api/link.js
+++ common/api/link.js
@@ -2,6 +2,7 @@ import Q from 'bluebird-q';
 import read from 'node-readability';
 import qs from 'qs';
 import { map, filter } from 'lodash';
+import parseLinks from '../lib/parse_links';
 import url from 'url';
 
 let { ABSTRACT_ENDPOINT } = process.env;
@@ -9,10 +10,15 @@ let { ABSTRACT_ENDPOINT } = process.env;
 export default (url) => {
   return Q.promise((resolve, reject) => {
     read(url, (err, article, meta) => {
-      console.log('err', err);
-      console.log('article', article);
-      console.log('meta', meta);
-      resolve(article);
+      if(err){
+        return reject(err);
+      }
+      resolve({
+        html: article.content,
+        title: article.title,
+        hrefs: parseLinks(article.content)
+      });
+      return article.close();
     })
   });
 }
