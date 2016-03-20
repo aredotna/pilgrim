981022ceb9c4385206da6e07d355fd9777d28437
Charles Broskoski <broskoski@gmail.com>
2016-03-19 20:39:11 -0400

    adds newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Pilgrim'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'dcc4267d6e51df81f76659e7fd271649018adc07',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
