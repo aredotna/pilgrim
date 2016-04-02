4038cc1b8236fde2105ddb424c331ce2ab1ae9b7
Charles Broskoski <broskoski@gmail.com>
2016-04-02 17:53:12 -0400

    adds style links

---
+++ server/home/examples.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    "url": "http://www.wired.com/2016/03/final-game-alphago-lee-sedol-big-deal-humanity/",
+    "title": "Why the Final Game Between AlphaGo and Lee Sedol Is Such a Big Deal for Humanity",
+  },
+  {
+    "url": "http://www.ribbonfarm.com/2016/02/11/minimum-viable-superorganism/",
+    "title": "Minimum Viable Superorganism",
+  },
+  {
+    "url": "http://www.thedailybeast.com/articles/2016/01/03/why-we-all-dream-of-being-jewel-thieves.html",
+    "title": "Why We All Dream of Being Jewel Thieves",
+  },
+]
