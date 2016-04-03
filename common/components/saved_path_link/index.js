c42a546c5ac689deaa62a17c52e54ff6ec8ce0de
Charles Broskoski <broskoski@gmail.com>
2016-04-03 16:58:05 -0400

    api and interface for building a path link

---
+++ common/components/saved_path_link/index.js
@@ -0,0 +1,44 @@
+import { default as React, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { generatePathLink } from '../../actions';
+import classNames from 'classnames';
+
+const { APP_URL } = process.env;
+
+class SavedPathLink extends React.Component {
+  render() {
+    const { path_url, onLinkClick } = this.props;
+
+    if(!path_url){
+      return (
+        <div className="saved-path-link">
+          <span onClick={onLinkClick}>Generate link</span>
+        </div>
+      );
+    }else{
+      const generatedLink = `${APP_URL}path/${path_url}`;
+      return (
+        <div className="saved-path-link">
+          <input type="text" value={generatedLink} />
+        </div>
+      );
+    }
+  }
+}
+
+
+const mapStateToProps = (state) => {
+  return {
+    path_url: state.path_url,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onLinkClick: () => {
+      dispatch(generatePathLink());
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SavedPathLink);
--- server/api/index.js
+++ server/api/index.js
@@ -1,18 +1,37 @@
 import express from 'express';
-import { take } from 'lodash';
 import cachedLink from '../../common/api/cached_link';
-import prefetchLinks from '../../common/lib/prefetch_links.js';
+import { savePath, getPath } from '../../common/api/path';
+import prefetchLinks from '../../common/lib/prefetch_links';
+import Q from 'bluebird-q';
 
 let app = express();
 
-app.get('/api/:url', (req, res, next) => {
-  cachedLink(req.params.url).then(results => {
-    res.json(results);
-    prefetchLinks(results.hrefs, 10);
-  }).catch(err => {
-    console.log('API ERROR | ', err.stack);
-    next();
-  });
-})
+app
+  .get('/api/:url', (req, res, next) => {
+    cachedLink(req.params.url).then(results => {
+      res.json(results);
+      prefetchLinks(results.hrefs, 10);
+    }).catch(err => {
+      console.log('API ERROR | ', err.stack);
+      next();
+    });
+  })
+  .post('/api/path', (req, res, next) => {
+    console.log('body', req.body);
+    savePath(req.body).then((response) => {
+      return res.json(response);
+    }).catch(err => {
+      console.log('API ERROR | ', err.stack);
+      next();
+    });
+  })
+  .get('/api/path/:id', (req, res, next) => {
+    getPath(req.params.id).then((response) => {
+      return res.json(response);
+    }).catch(err => {
+      console.log('API ERROR | ', err.stack);
+      next();
+    });
+  })
 
 export default app;
