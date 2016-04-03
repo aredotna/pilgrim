4ae7405fc3ebf9b3f4e4364f5cc4c5b5021eb763
Charles Broskoski <broskoski@gmail.com>
2016-04-03 16:57:47 -0400

    rootLink -> root_link

--- common/reducers/index.js
+++ common/reducers/index.js
@@ -1,18 +1,20 @@
 import { combineReducers } from 'redux';
-import rootLink from './rootLink';
+import root_link from './root_link';
 import path from './path';
 import links from './links';
 import loading from './loading';
 import preview_url from './preview_url';
 import hovered_link from './hovered_link';
+import path_url from './path_url';
 
 const rootReducer = combineReducers({
-  rootLink,
+  root_link,
   path,
   links,
   loading,
   preview_url,
-  hovered_link
+  hovered_link,
+  path_url
 });
 
 export default rootReducer;
--- server/explore/index.js
+++ server/explore/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
+import Q from 'bluebird-q';
 import React from 'react';
-import { take } from 'lodash';
+import { take, map, find, keys } from 'lodash';
 import { renderToStaticMarkup } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import configureStore from '../../common/store/configure_store';
@@ -8,39 +9,79 @@ import { isURL } from 'validator';
 import Explore from './explore.js';
 import render from '../render.js';
 import cachedLink from '../../common/api/cached_link';
-import prefetchLinks from '../../common/lib/prefetch_links.js';
+import { getPath } from '../../common/api/path';
+import prefetchLinks from '../../common/lib/prefetch_links';
 
 let app = express();
 
-app.get('/:url', (req, res, next) => {
-  const url = req.params.url;
-  cachedLink(url, req).then( results => {
-    // set initial state as fetched info
-    let store = configureStore({
-      rootLink: url,
-      links: {
-        [url]: results
-      },
-      preview_url: false,
-      hovered_link: false,
-      path: [url],
-      loading: false
-    });
+const { APP_URL } = process.env;
 
-    // Render the component to a raw string
-    const html = renderToStaticMarkup(
-      <Provider store={store}>
-        <Explore />
-      </Provider>
-    );
+app
+  .get('/:url', (req, res, next) => {
+    const url = req.params.url;
+    cachedLink(url, req).then( results => {
+      // set initial state as fetched info
+      const store = configureStore({
+        root_link: url,
+        links: {
+          [url]: results
+        },
+        preview_url: false,
+        hovered_link: false,
+        path: [url],
+        loading: false,
+        path_url: false
+      });
 
-    // Render our boilerplate page with HTML and the initial state set
-    res.send(render(html, store.getState(), results.title));
-    prefetchLinks(results.hrefs);
-  }).catch( err => {
-    console.log('ERROR | ', err.stack)
-    next()
+      // Render the component to a raw string
+      const html = renderToStaticMarkup(
+        <Provider store={store}>
+          <Explore />
+        </Provider>
+      );
+
+      // Render our boilerplate page with HTML and the initial state set
+      res.send(render(html, store.getState(), results.title));
+      prefetchLinks(results.hrefs);
+    }).catch( err => {
+      console.log('ERROR | ', err.stack)
+      next()
+    })
   })
-})
+  .get('/path/:id', (req, res, next) => {
+    const id = req.params.id;
+    getPath(id).then( pathData => {
+      Q.all(map(pathData.path, url => cachedLink(url))).then((results) => {
+        let links = {};
+        map(pathData.path, (url)=>{
+          let linkData = find(results, { url: url });
+          links[url] = linkData;
+        });
+
+        const store = configureStore({
+          root_link: pathData.root_link,
+          links: links,
+          preview_url: false,
+          hovered_link: false,
+          path: pathData.path,
+          loading: false,
+          path_url: id
+        });
+
+        // Render the component to a raw string
+        const html = renderToStaticMarkup(
+          <Provider store={store}>
+            <Explore />
+          </Provider>
+        );
+
+        // Render our boilerplate page with HTML and the initial state set
+        return res.send(render(html, store.getState(), results[0].title));
+      });
+    }).catch( err => {
+      console.log('ERROR | ', err.stack)
+      next()
+    });
+  })
 
 export default app;
